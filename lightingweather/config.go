f7474de3d33358188adad24426eb5b8a9603c542
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2024-11-28 06:11:40 -0700

    lightingweather: split out config and main file

---
+++ lightingweather/config.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	_ "embed"
+	"errors"
+	"fmt"
+	"os"
+	"sort"
+    "gopkg.in/yaml.v3"
+
+	hue "github.com/collinux/gohue"
+)
+
+var errInvalidColor = errors.New("invalid color")
+
+type color struct {
+    Color string `yaml:"color"`
+    Threshold int `yaml:"threshold"`
+}
+
+var colorTranslate = map[string]*[2]float32{
+    "blue": hue.BLUE,
+    "cyan": hue.CYAN,
+    "green": hue.GREEN,
+    "orange": hue.ORANGE,
+    "pink": hue.PINK,
+    "purple": hue.PURPLE,
+    "red": hue.RED,
+    "white": hue.WHITE,
+    "yellow": hue.YELLOW,
+}
+
+type config struct {
+    Unit string `yaml:"unit"`
+    Lang string `yaml:"lang"`
+    Location string `yaml:"location"`
+    HueID string `yaml:"hue_id"`
+    HueIPAddress string `yaml:"hue_ip_address"`
+    OWMAPIKey string `yaml:"owm_api_key"`
+    LightName string `yaml:"light_name"`
+    MaxColor string `yaml:"max_color"`
+    Colors []color `yaml:"colors"`
+}
+
+func (cfg *config) sortColorRange() *config {
+    sort.Slice(cfg.Colors, func(i, j int) bool {
+        return cfg.Colors[i].Threshold < cfg.Colors[j].Threshold
+    })
+    return cfg
+}
+
+func newConfig(configFile string) (*config, error) {
+    cf, err := os.Open(configFile)
+    if err != nil {
+        return nil, err
+    }
+    defer cf.Close()
+
+    var cfg config
+
+    if err := yaml.NewDecoder(cf).Decode(&cfg); err != nil {
+        return nil, err
+    }
+
+    for _, cl := range cfg.Colors {
+        if _, ok := colorTranslate[cl.Color]; !ok {
+            return nil, fmt.Errorf("%w:%s", errInvalidColor, cl.Color)
+        }
+    }
+
+    //Override OWM API Key with env var
+    if ownKey, ok := os.LookupEnv("OWM_API_KEY"); ok {
+        cfg.OWMAPIKey = ownKey
+    }
+    //Override HUE ID with env var
+    if hueID, ok := os.LookupEnv("HUE_ID"); ok {
+        cfg.HueID = hueID
+    }
+
+    return cfg.sortColorRange(), nil
+}
+
+func pickColor(cfg *config, curTemp int) *[2]float32 {
+    for _, cl := range cfg.Colors {
+        if curTemp < cl.Threshold {
+            return colorTranslate[cl.Color]
+        }
+    }
+
+    return colorTranslate[cfg.MaxColor]
+}
--- lightingweather/lightingweather.go
+++ lightingweather/lightingweather.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	_ "embed"
-	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"math"
 	"net/http"
 	"os"
-	"sort"
 	"time"
-    "gopkg.in/yaml.v3"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -23,85 +19,6 @@ import (
 
 //go:embed rootPage.html
 var rootPageHTML []byte
-
-var errInvalidColor = errors.New("invalid color")
-
-type color struct {
-    Color string `yaml:"color"`
-    Threshold int `yaml:"threshold"`
-}
-
-var colorTranslate = map[string]*[2]float32{
-    "blue": hue.BLUE,
-    "cyan": hue.CYAN,
-    "green": hue.GREEN,
-    "orange": hue.ORANGE,
-    "pink": hue.PINK,
-    "purple": hue.PURPLE,
-    "red": hue.RED,
-    "white": hue.WHITE,
-    "yellow": hue.YELLOW,
-}
-
-type config struct {
-    Unit string `yaml:"unit"`
-    Lang string `yaml:"lang"`
-    Location string `yaml:"location"`
-    HueID string `yaml:"hue_id"`
-    HueIPAddress string `yaml:"hue_ip_address"`
-    OWMAPIKey string `yaml:"owm_api_key"`
-    LightName string `yaml:"light_name"`
-    MaxColor string `yaml:"max_color"`
-    Colors []color `yaml:"colors"`
-}
-
-func (cfg *config) sortColorRange() *config {
-    sort.Slice(cfg.Colors, func(i, j int) bool {
-        return cfg.Colors[i].Threshold < cfg.Colors[j].Threshold
-    })
-    return cfg
-}
-
-func newConfig(configFile string) (*config, error) {
-    cf, err := os.Open(configFile)
-    if err != nil {
-        return nil, err
-    }
-    defer cf.Close()
-
-    var cfg config
-
-    if err := yaml.NewDecoder(cf).Decode(&cfg); err != nil {
-        return nil, err
-    }
-
-    for _, cl := range cfg.Colors {
-        if _, ok := colorTranslate[cl.Color]; !ok {
-            return nil, fmt.Errorf("%w:%s", errInvalidColor, cl.Color)
-        }
-    }
-
-    //Override OWM API Key with env var
-    if ownKey, ok := os.LookupEnv("OWM_API_KEY"); ok {
-        cfg.OWMAPIKey = ownKey
-    }
-    //Override HUE ID with env var
-    if hueID, ok := os.LookupEnv("HUE_ID"); ok {
-        cfg.HueID = hueID
-    }
-
-    return cfg.sortColorRange(), nil
-}
-
-func pickColor(cfg *config, curTemp int) *[2]float32 {
-    for _, cl := range cfg.Colors {
-        if curTemp < cl.Threshold {
-            return colorTranslate[cl.Color]
-        }
-    }
-
-    return colorTranslate[cfg.MaxColor]
-}
 
 func getCurrentTemperature(cfg *config) (int, error) {
     w, err := owm.NewCurrent(cfg.Unit, cfg.Lang, cfg.OWMAPIKey)
